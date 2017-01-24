78a6011c11ecdd27e356637cc132fcc48768e312
Pierre Dulac <dulacpier@gmail.com>
2017-01-24 19:17:39 +0100

    Add options to the commandline

--- previewer/cli.py
+++ previewer/cli.py
@@ -9,46 +9,36 @@ from .previewer import PreviewGeneratorNative, PreviewGeneratorOpenCV
 
 @click.command()
 @click.argument('file_path', type=click.Path(exists=True))
-def main(file_path):
-    click.echo("Image will be loaded at path '{}'".format(file_path))
-    
-    preview_size = (1000,750)
+@click.option('--output', type=click.Path(), default='output.jpg')
+@click.option('--preview-height', type=int, default=750)
+@click.option('--preview-width', type=int, default=1000)
+@click.option('--no-opencv', type=bool, default=False)
+def main(file_path, output, preview_height, preview_width, no_opencv):
+    click.echo(click.style("Image will be loaded from path '{}'".format(file_path), fg='blue'))
 
-    ## OpenCV implementation
+    preview_size = (preview_width, preview_height)
+    click.echo(click.style("Preview will be generated at size '{}'".format(preview_size), fg='blue'))
 
-    click.echo("Loading image (opencv)...", nl=False)
+    click.echo("Loading image...", nl=False)
     t0 = time.time()
-    p = PreviewGeneratorOpenCV(file_path, preview_size=preview_size)
+    if not no_opencv:
+        p = PreviewGeneratorOpenCV(file_path, preview_size=preview_size)
+    else:
+        click.echo(click.style("Warning: very unefficient implementation, it might take more than a minute", fg='orange'))
+        p = PreviewGeneratorNative(file_path, preview_size=preview_size)
     click.echo(" executed in {:.3f}s".format(time.time() - t0))
     
-    click.echo("Generating preview (opencv)...", nl=False)
+    click.echo("Generating preview...", nl=False)
     t0 = time.time()
     out = p.generate()
     click.echo(" executed in {:.3f}s".format(time.time() - t0))
 
     click.echo("Saving the preview...", nl=False)
     t0 = time.time()
-    cv2.imwrite('out_1.jpg', out)
+    cv2.imwrite(output, out)
     click.echo(" executed in {:.3f}s".format(time.time() - t0))
 
-    ## Native implementation
-
-    click.echo("Loading image (native)...", nl=False)
-    t0 = time.time()
-    p = PreviewGeneratorNative(file_path, preview_size=preview_size)
-    click.echo(" executed in {:.3f}s".format(time.time() - t0))
-
-    click.echo("Generating preview (native)...", nl=False)
-    t0 = time.time()
-    out = p.generate()
-    click.echo(" executed in {:.3f}s".format(time.time() - t0))
-
-    click.echo("Saving the preview...", nl=False)
-    t0 = time.time()
-    out.save('out_2.jpg')
-    click.echo(" executed in {:.3f}s".format(time.time() - t0))
-
-    click.echo("Done!")
+    click.echo(click.style("Done!", fg='green'))
 
 
 if __name__ == "__main__":
