d9595423a8df7cb90da3a83103f9765bfc3df5b0
Pierre Dulac <dulacpier@gmail.com>
2017-01-25 11:28:03 +0100

    Remove useless code in the setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,6 @@
 
 from setuptools import setup
 
-with open('README.rst') as readme_file:
-    readme = readme_file.read()
-
-with open('HISTORY.rst') as history_file:
-    history = history_file.read()
 
 requirements = [
     'click==6.7',
@@ -22,16 +17,15 @@ test_requirements = [
 setup(
     name='previewer',
     version='0.1.0',
-    description="Generate a preview of the 3660 image.",
-    long_description=readme + '\n\n' + history,
+    description="Generate a preview of the 360 image.",
     author="Pierre Dulac",
     author_email='pierre@dulaccc.me',
-    url='https://github.com/dulaccc/previewer',
+    url='https://github.com/Photonomie/previewer',
     packages=[
         'previewer',
     ],
     package_dir={
-        '': '${CMAKE_CURRENT_SOURCE_DIR}',
+        # '': '${CMAKE_CURRENT_SOURCE_DIR}',
         'previewer': 'previewer'
     },
     entry_points={
