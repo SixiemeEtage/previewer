adef4cfad9638d239745d3fc899e0c9a7b7d28cd
Pierre Dulac <dulacpier@gmail.com>
2017-01-25 11:56:54 +0100

    Reduce the compatibilities to py27 and py36

--- .travis/install.sh
+++ .travis/install.sh
@@ -13,33 +13,13 @@ if [[ "$(uname -s)" == 'Darwin' ]]; then
     eval "$(pyenv init -)"
 
     case "${TOXENV}" in
-        py26)
-            curl -O https://bootstrap.pypa.io/get-pip.py
-            python get-pip.py --user
-            ;;
         py27)
             curl -O https://bootstrap.pypa.io/get-pip.py
             python get-pip.py --user
             ;;
-        py33)
-            pyenv install 3.3.6
-            pyenv global 3.3.6
-            ;;
-        py34)
-            pyenv install 3.4.2
-            pyenv global 3.4.2
-            ;;
-        py35)
-            pyenv install 3.5.0
-            pyenv global 3.5.0
-            ;;
-        pypy)
-            pyenv install pypy-4.0.1
-            pyenv global pypy-4.0.1
-            ;;
-        pypy3)
-            pyenv install pypy3-2.4.0
-            pyenv global pypy3-2.4.0
+        py36)
+            pyenv install 3.6.0
+            pyenv global 3.6.0
             ;;
         docs)
             curl -O https://bootstrap.pypa.io/get-pip.py
@@ -49,16 +29,6 @@ if [[ "$(uname -s)" == 'Darwin' ]]; then
     pyenv rehash
     python -m pip install --user virtualenv
 else
-    # temporary pyenv installation to get latest pypy before container infra upgrade
-    # now using the -latest because of a segfault bug we're encountering in 2.6.1
-    if [[ "${TOXENV}" == "pypy" ]]; then
-        git clone https://github.com/yyuu/pyenv.git ~/.pyenv
-        PYENV_ROOT="$HOME/.pyenv"
-        PATH="$PYENV_ROOT/bin:$PATH"
-        eval "$(pyenv init -)"
-        pyenv install pypy-4.0.1
-        pyenv global pypy-4.0.1
-    fi
     pip install virtualenv
 fi
 
